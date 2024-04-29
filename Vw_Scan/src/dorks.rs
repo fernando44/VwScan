b3624a260af7bba72a292d4519beda1549412de5
Fernando Chiareli Ferreira <fernando.ferreira.chiareli@hotmail.com>
2024-04-29 14:02:06 -0300

    update

--- Vw_Scan/src/dorks.rs
+++ Vw_Scan/src/dorks.rs
@@ -1,6 +1,7 @@
 use std::fs::read_to_string;
-use std::io::{Error, ErrorKind};
+use std::io::{Error, ErrorKind, Write};
 use regex::Regex;
+use std::fs::File;
 use reqwest;
 
 /*
@@ -10,9 +11,11 @@ use reqwest;
     verificar funcionalidade do codigo
 */
 pub(crate) async fn dorks(site: &str) -> Result<(), Error> {
+    println!("Função buscas relevantes na internet iniciada\n");
     // Lendo o conteúdo do arquivo dorks.txt
     let dorks = read_to_string("dorks.txt").expect("Could not read the file dorks.txt.");
-
+    println!("\tCriando arquivo dorksUrls.txt");
+    let mut dorks_file = File::create("dorksUrls.txt")?;
     for dork in dorks.lines() {
         // Construindo a URL de busca com o dork e o site
         let url = format!("https://www.google.com/search?q={}+site:{}", dork, site);
@@ -29,16 +32,16 @@ pub(crate) async fn dorks(site: &str) -> Result<(), Error> {
         if response.status().is_success() {
             // Lendo o corpo da resposta como texto
             let body_result = response.text().await.map_err(|err| Error::new(ErrorKind::Other, err))?;
-            
-            let re = Regex::new(r#"(http[^"]*meusite[^"]*)""#).unwrap();//r#"href="([^"]*meusite\.br[^"]*)""#
-            
+            let re_str = format!(r#"(http[^"]*{}[^"]*)""#, site);
+            let re = Regex::new(&re_str).unwrap();
             for cap in re.captures_iter(&body_result) {
-                println!("Link encontrado: {}", &cap[1]);
+                writeln!(dorks_file, "{}", &cap[1])?;
             }
         } else {
-            println!("Dork: {}\nRequest failed with status: {}", dork, response.status());
+            println!("Error: {}\nRequest failed with status: {}", dork, response.status());
         }
     }
-
+    println!("\tDados Separados");
+    println!("Função buscas relevantes na internet finalizada\n");
     Ok(())
 }
